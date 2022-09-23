656db81f448b79c966123edcb32b3ce4b4b2bf80
Axurit <hjunca@skalv-studio.fr>
2022-09-23 15:24:01 +0200

    reset password function in security controller with templates and email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -47,6 +47,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $is_verified = false;
 
+    #[ORM\Column(type: 'string', length: 100)]
+    private $resetToken;
+
     public function __construct()
     {
         $this->messages = new ArrayCollection();
@@ -180,6 +183,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $this->is_verified = $is_verified;
 
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->resetToken;
+    }
+
+    public function setResetToken(?string $resetToken):self
+    {
+        $this->resetToken = $resetToken;
+        
         return $this;
     }
 }
