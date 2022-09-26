9a23a04d589d7e865e698150755274505c633c9e
Axurit <hjunca@skalv-studio.fr>
2022-09-26 15:06:52 +0200

    allow add and remove images on edit form

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -5,15 +5,16 @@ namespace App\Controller;
 use App\Entity\Image;
 use App\Entity\Trick;
 use App\Form\TrickType;
+use Doctrine\ORM\Mapping\Entity;
 use App\Repository\ImageRepository;
 use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/trick')]
 class TrickController extends AbstractController
@@ -113,6 +114,7 @@ class TrickController extends AbstractController
 
         return $this->render('trick/edit.html.twig', [
             'trick' => $form->createView(),
+            'images' => $trick->getImages(),
             'controller_name' => 'TrickController',
         ]);
     }
