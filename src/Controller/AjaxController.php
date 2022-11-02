ba88ec5cf0c22f0bd941d3222c80737a2de30971
Axurit <hjunca@skalv-studio.fr>
2022-11-02 18:30:23 +0100

    rework pagination

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -3,34 +3,62 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AjaxController extends AbstractController
 {
     #[Route('/ajax/trick', name: 'app_ajax_trick')]
     public function ajaxAction(Request $request, TrickRepository $trickRepository): Response
     {
-        $tricks = $trickRepository->findAll();
+        // get page number
+        $page = (int)$request->query->get("page", 2);
+        // dd($page);
+        
+        $tricks = $trickRepository->getTricks($page);
+        // dd($tricks);
 
-        if($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
-            $jsonData = array();
-            $idx = 0;
+        $encoders = [new JsonEncoder()]; 
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
 
-            foreach($tricks as $trick) {
-                $temp = array(
-                    'name' => $trick->getName()
-                );
-                $jsonData[$idx++] = $temp;
-            }
-            return new JsonResponse($jsonData);
-        } else {
-            return $this->render('trick/_ajax.html.twig', [
-                'controller_name' => 'AjaxController',
-            ]);
-        }
+        $jsonObject = $serializer->serialize($tricks, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            },
+        ]);
+
+        return new Response($jsonObject, 200, ['Content-Type' => 'application/json']);
+        
+
+        // return $this->json([
+        //     'success' => true,
+        //     'data' => $tricks
+        // ]);
+
+        // if($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
+        //     $jsonData = array();
+        //     $idx = 0;
+
+        //     foreach($tricks as $trick) {
+        //         $temp = array(
+        //             'element' => $this->render('trick/_content.html.twig', [
+        //                 'tricks' => $tricks,
+        //             ])
+        //         );
+        //         $jsonData[$idx++] = $temp;
+        //     }
+        //     return new JsonResponse($jsonData);
+        // } else {
+        //     return $this->render('trick/_ajax.html.twig', [
+        //         'controller_name' => 'AjaxController',
+        //     ]);
+        // }
     }
 }
--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Trick;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -37,6 +38,62 @@ class TrickRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+    private function getTrickQueryBuilder(){
+        $queryBuilder = $this->createQueryBuilder('t');
+
+        return $queryBuilder;
+    }
+
+
+    public function getTricks($page){
+        $pageSize = 6;
+		$firstResult = ($page - 1) * $pageSize;
+
+		$queryBuilder = $this->getTrickQueryBuilder();
+		
+		// Set the returned page
+		$queryBuilder->setFirstResult($firstResult);
+		$queryBuilder->setMaxResults($pageSize);
+		
+		// Generate the Query
+		$query = $queryBuilder->getQuery();
+
+        //Generate the Paginator
+        $paginator = new Paginator($query, true);
+        return $paginator;
+    }
+
+        
+    /**
+     * getPaginatedTricks
+     *
+     * @param  mixed $page
+     * @param  mixed $limit
+     * @return void
+     */
+    public function getPaginatedTricks($page, $limit)
+    {
+        $query = $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt')
+            ->setFirstResult(($page * $limit) - $limit)
+            ->setMaxResults($limit);
+
+            return $query->getQuery()->getResult();
+    }
+    
+    /**
+     * Return number of tricks
+     *
+     * @return void
+     */
+    public function getTotalTricks()
+    {
+        $query = $this->createQueryBuilder('t')
+            ->select('COUNT(t)');
+
+        return $query->getQuery()->getSingleScalarResult();
     }
 
 //    /**
