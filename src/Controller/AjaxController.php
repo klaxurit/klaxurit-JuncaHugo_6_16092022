83a12aaa449d91c18708004030108662c08a2a3e
Axurit <hjunca@skalv-studio.fr>
2022-11-04 16:09:10 +0100

    rework on ajaxAction

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -7,7 +7,6 @@ use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +14,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AjaxController extends AbstractController
 {
     #[Route('/ajax/trick', name: 'app_ajax_trick')]
-    public function ajaxAction(Request $request, TrickRepository $trickRepository): Response
+    public function ajaxAction(
+        Request $request, 
+        TrickRepository $trickRepository,
+        bool $noMoreTricks = false
+        ): Response
     {
         // get page number
         $page = (int)$request->query->get("page");
@@ -27,6 +30,9 @@ class AjaxController extends AbstractController
         $encoders = [new JsonEncoder()]; 
         $normalizers = [new ObjectNormalizer()];
         $serializer = new Serializer($normalizers, $encoders);
+
+        $maxResults = $trickRepository->getTotalTricks();
+        // dd($maxResults);
 
         $jsonObject = $serializer->serialize($tricks, 'json', [
             'circular_reference_handler' => function ($object) {
