b7e325906a4589aa20b3476cb478752a764c0739
Axurit <hjunca@skalv-studio.fr>
2022-12-14 11:49:52 +0100

    fix edit trick error show

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -214,6 +214,45 @@ class TrickController extends AbstractController
         }
     }
 
+    #[Route('/ajax/trick', name: 'app_ajax_trick')]
+    /**
+     * Create jsonObject from requested data
+     *
+     * @param Request $request
+     * @param TrickRepository $trickRepository
+     * @param UserInterface|null $user
+     * @return Response
+     */
+    public function ajaxAction(
+        Request $request,
+        TrickRepository $trickRepository,
+        UserInterface $user = null
+    ): JsonResponse {
+        return $this->json([
+            'total' => $trickRepository->getTotalTricks(),
+            'tricks' => $trickRepository->getTricks((int)$request->query->get("page")),
+            'current_user' => $user ? $user : "",
+        ], 200, [], ['groups' => 'trick:read']);
+    }
+
+    #[Route('/ajax/comment', name: 'app_ajax_comment')]
+    /**
+     * Create jsonObject from requested data
+     *
+     * @param Request $request
+     * @param UserMessageRepository $userMessage
+     * @return Response
+     */
+    public function ajaxCommentAction(
+        Request $request,
+        UserMessageRepository $userMessage,
+    ): Response {
+        return $this->json([
+            'comments' => $userMessage->getCommentsOfATrick((int)$request->query->get("page"), (int)$request->query->get("trickid")),
+            'total' => $userMessage->getTotalComments()
+        ], 200, [], ['groups' => 'comment:read']);
+    }
+
     public function mediaTrickManager(
         object $form,
         Trick $trick,
